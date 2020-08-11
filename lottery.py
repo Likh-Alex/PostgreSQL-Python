505fa9598ccb9ffeb35407c07265408bd779913a
Alex <67925366+Likh-Alex@users.noreply.github.com>
2020-08-11 11:22:59 +0300

    Add files via upload

--- lottery.py
+++ lottery.py
@@ -1,7 +1,7 @@
+import random
+
 # User can pick 6 numbers
-# Lottery calculates 6 random numbers
-# Then we match the user number to the lottery numbers
-# Calculate the winnings based on how many the user matched
+
 
 def get_players_number():
     number_csv = input("Enter your 6 numbers, separated by commas: ")
@@ -9,5 +9,13 @@ def get_players_number():
     numbers = number_csv.split(",")
     integer_set = {int(number) for number in numbers}
     return integer_set
-
+# Lottery calculates 6 random numbers
+def create_lottery_numbers():
+    lottery_numbers =set()
+    for i in range(6):
+        lottery_numbers.add(random.randint(0,1000000))
+    return lottery_numbers
+# Then we match the user number to the lottery numbers
+# Calculate the winnings based on how many the user matched
 print(get_players_number())
+
