7ff184dc1b35cbc3d644a5b8df0e0baf863962e3
Alex <67925366+Likh-Alex@users.noreply.github.com>
2020-08-11 15:49:20 +0300

    Added menu for user navigation

--- registration_app.py
+++ registration_app.py
@@ -7,7 +7,7 @@ def create_student():
     student = {'name':student_name,
                'marks': []}
     # return the dictionary
-    student_list.append(student)
+    return student
 
 def add_marks(student, mark):
     #add a mark to the student dictionary
@@ -29,10 +29,43 @@ def student_details(student):
 
 def print_all_students(students):
     # print out the string that tells the user the info abouth the student for every student in the list
-    for student in student_list:
+    for i, student in enumerate(students):
+        print(f"ID : {i}")
         print(student_details(student))
 
 
+def menu():
+    #add a student to a student list
+    #add a mark to a student
+    #Print a list of students
+    #Exit the application
+    selection = input("Enter 'p' to print the list of all students,"
+                      " 's' to add a new student,"
+                      " 'm' to add marks for the student,"
+                      " 'q' to exit"
+                      "\nEnter your selection: ...")
+    while selection != 'q':
+        if selection == 'p':
+            print_all_students(student_list)
+            print("No students in the list")
+        elif selection == 's':
+            student_list.append(create_student())
+        elif selection == 'm':
+            student_id = int(input("Enter the student ID to a mark to: "))
+            student = student_list[student_id]
+            new_mark = int(input("Enter a new mark to be added: "))
+            add_marks(student, new_mark)
 
-new_student = create_student()
-student_details(new_student)
+        selection = input("Enter 'p' to print the list of all students,"
+                      " 's' to add a new student,"
+                      " 'm' to add marks for the student,"
+                      " 'q' to exit"
+                      "\nEnter your selection: ...")
+
+
+
+
+
+
+
+menu()
